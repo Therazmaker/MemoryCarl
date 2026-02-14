d75ccb5b7617589a26dbe999a7eb406a70d10e7f
Therazmaker <63662119+Therazmaker@users.noreply.github.com>
2026-02-13 23:13:10 -0500

    public directory

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME="memorycarl-v1";
-const ASSETS=["/","/index.html","/src/style.css","/src/main.js","/manifest.webmanifest"];
-self.addEventListener("install",e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
