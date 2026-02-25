733955d358525d1a371cbfccc563ef3df1e7ce36
Therazmaker <63662119+Therazmaker@users.noreply.github.com>
2026-02-25 15:19:49 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { initFootballLab } from "./footballLab_v6.js";
+import { initFootballLab } from "./footballLab_v6d.js";
 
 /* ===== PWA Rescue / Reset =====
    Si la app se queda pegada (cache/estado viejo), abre:
