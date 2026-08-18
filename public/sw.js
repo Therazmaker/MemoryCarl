955e566599e0014ad7a45763a044feca97ec9c42
Therazmaker <therazmaker@users.noreply.github.com>
2026-08-17 22:11:17 -0500

    chore: bump script version parameter to v=2003 and sw cache name to gemini23

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="memorycarl-v2-gemini22";
+const CACHE_NAME="memorycarl-v2-gemini23";
 const ASSETS=["/","/index.html","/src/style.css","/src/main.js","/manifest.webmanifest"];
 
 self.addEventListener("install",e=>{
