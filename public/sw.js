696896fa745aa5b664a2e747813a40ae63ef3df9
Therazmaker <therazmaker@users.noreply.github.com>
2026-08-17 21:39:10 -0500

    chore: bump main.js script version parameter and sw cache name to force client update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="memorycarl-v2-gemini20";
+const CACHE_NAME="memorycarl-v2-gemini21";
 const ASSETS=["/","/index.html","/src/style.css","/src/main.js","/manifest.webmanifest"];
 
 self.addEventListener("install",e=>{
